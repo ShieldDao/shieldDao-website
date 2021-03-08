8da464b999378732359952d9c58be77c36b7059d
Amin A. Rezapour <amin.pe94@gmail.com>
2021-03-08 13:20:25 +0330

    Added Partners Logos

--- src/components/home/partners.js
+++ src/components/home/partners.js
@@ -1,32 +1,81 @@
+import styled from 'styled-components';
+
+const LogosList = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 -15px;
+  flex-wrap: wrap;
+
+  .partner-logo {
+    padding: 0 15px;
+
+    img {
+      width: 150px;
+      height: auto;
+      line-height: 1;
+      vertical-align: middle;
+      display: inline-block;
+
+      &.smaller {
+        width: 80px;
+      }
+    }
+  }
+`;
+
 const HomePartners = () => {
   return (
     <section style={{ marginBottom: '80px' }} className="roadmap" id="roadmap">
       <div className="container">
         <div className="mb-4">
           <h2 className="text-center text-slate-dark heading-bigger mb-4">
-            ShieldDAO Infographic
+            ShieldDAO Partners
           </h2>
 
           <div className="row align-items-center">
-            <div className="col-12 col-md-6">
-              <p className="text-center">
-                <img alt="roadmap" className="present" src="/svg/rmap-bl.svg" />
-              </p>
-            </div>
-
-            <div className="col-12 col-md-6">
-              <h2 className="text-center col-12 text-slate-dark mb-4">
-                Tokenomics
-              </h2>
-
-              <p className="text-center">
+            <LogosList className="col-12">
+              <a
+                className="partner-logo"
+                href="https://nuls.io"
+                title="Nuls"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                <img src="/svg/logo-nuls.svg" alt="Nuls" />
+              </a>
+              <a
+                className="partner-logo"
+                href="https://halborn.com/"
+                title="Halborn"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                <img src="/svg/halborn_logo.svg" alt="Halborn" />
+              </a>
+              <a
+                className="partner-logo"
+                href="https://www.bamboodefi.com/"
+                title="Bamboo"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
                 <img
-                  className="present"
-                  alt="tokenomics"
-                  src="/svg/shdtoken-bl.svg"
+                  className="smaller"
+                  src="/images/bamboo-head.png"
+                  alt="Bamboo"
                 />
-              </p>
-            </div>
+              </a>
+              <a
+                className="partner-logo"
+                href="https://nerve.network/"
+                title="Nerve"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                <img src="/svg/logo.a3a86c47.svg" alt="Nerve" />
+              </a>
+            </LogosList>
           </div>
         </div>
       </div>
@@ -34,4 +83,4 @@ const HomePartners = () => {
   );
 };
 
-export default HomeInfoGraphics;
+export default HomePartners;
